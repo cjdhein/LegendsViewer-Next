f184d51d5d7d11ed56b7b5e33753bd9a9e1d4081
cjdhein <cody.dhein@gmail.com>
2025-03-16 12:32:22 -0500

    Store and retrieve the last used directory

--- LegendsViewer.Frontend/legends-viewer-frontend/vite.config.ts
+++ LegendsViewer.Frontend/legends-viewer-frontend/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    outDir: `C:\\Program Files (x86)\\Steam\\steamapps\\common\\Dwarf Fortress\\LegendsViewer\\legends-viewer-frontend\\dist`,
+    emptyOutDir: true,
+  },
   plugins: [vue()],
 })
